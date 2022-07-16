672956cae02814d08aa1a472476a2cc493091318
mfthdkr <50250549+mfthdkr@users.noreply.github.com>
2022-07-16 20:41:39 +0300

    Validation in view pages.

--- StokTakip/Controllers/UrunlerController.cs
+++ StokTakip/Controllers/UrunlerController.cs
@@ -18,9 +18,9 @@ namespace StokTakip.Controllers
             {
                 if (product.UrunKategori == null)
                 {
-                    product.Kategoriler = new Kategoriler {KategoriAd = "Kategori Yoktur."};
+                    product.Kategoriler = new Kategoriler { KategoriAd = "Kategori Yoktur." };
                 }
-                
+
             }
             return View(productList);
         }
@@ -51,6 +51,8 @@ namespace StokTakip.Controllers
         public ActionResult Create(Urunler urunler)
         {
             var kategori = dbStokTakipEntities.Kategoriler.Where(k => k.KategoriId == urunler.Kategoriler.KategoriId).FirstOrDefault();
+
+
             urunler.Kategoriler = kategori;
 
             dbStokTakipEntities.Urunler.Add(urunler);
@@ -74,11 +76,11 @@ namespace StokTakip.Controllers
 
             List<SelectListItem> kategoriler =
                 (from kategori in dbStokTakipEntities.Kategoriler.ToList()
-                    select new SelectListItem()
-                    {
-                        Text = kategori.KategoriAd,
-                        Value = kategori.KategoriId.ToString()
-                    }).ToList();
+                 select new SelectListItem()
+                 {
+                     Text = kategori.KategoriAd,
+                     Value = kategori.KategoriId.ToString()
+                 }).ToList();
 
             ViewBag.kategoriler = kategoriler;
 
@@ -88,15 +90,18 @@ namespace StokTakip.Controllers
         [HttpPost]
         public ActionResult Update(Urunler urun)
         {
+
             var kategori = dbStokTakipEntities.Kategoriler.Where(p => p.KategoriId == urun.Kategoriler.KategoriId)
                 .FirstOrDefault();
             urun.UrunKategori = kategori?.KategoriId;
 
+
+
             var productToUpdate = dbStokTakipEntities.Urunler.Find(urun.UrunId);
             productToUpdate.UrunAd = urun.UrunAd;
             productToUpdate.UrunKategori = urun.UrunKategori;
-            productToUpdate.Fiyat =urun.Fiyat;
-            productToUpdate.Marka =urun.Marka;
+            productToUpdate.Fiyat = urun.Fiyat;
+            productToUpdate.Marka = urun.Marka;
             productToUpdate.Stok = urun.Stok;
 
             dbStokTakipEntities.SaveChanges();
