06e203d28d87aecc4674f6264c1b73cbcf33a0fc
mfthdkr <50250549+mfthdkr@users.noreply.github.com>
2022-07-19 14:40:18 +0300

    Popup, Sipariş verme

---
+++ StokTakip/Controllers/SalesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StokTakip.Models.EntityFramework;
+
+namespace StokTakip.Controllers
+{
+    public class SalesController : Controller
+    {
+        private DbStokTakipEntities dbStokTakipEntities = new DbStokTakipEntities();
+        // GET: Sales
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateSale(Satislar satis)
+        {
+            dbStokTakipEntities.Satislar.Add(satis);
+            dbStokTakipEntities.SaveChanges();
+
+            return View("Index");
+        }
+    }
+}
